82ec8a311a978e2fed0618d3ed0fe8e207301095
KseniaKuznetsova01 <ksenks.one@gmail.com>
2019-11-13 19:33:26 +0700

    part 4

--- case4.py
+++ case4.py
@@ -30,6 +30,8 @@ for i in text:
     if i == 'а' or i == 'у' or i == 'е' or i == 'о' or i == 'ю' or i == 'и' \
             or i == 'я' or i == 'э' or i == 'ё' or i == 'ы':
         syllables += 1
+    elif i == 'a' or i == 'e' or i == 'i' or i == 'o' or i == 'u' or i == 'y':
+        syllables += 1
 
 print(lc.SYLLABLES, syllables)
 
@@ -49,3 +51,6 @@ elif 50 < number_fre <= 80:
     print(lc.M_EASY)
 else:
     print(lc.EASY)
+from textblob.sentiments import NaiveBayesAnalyzer
+blob = TextBlob(text, analyzer=NaiveBayesAnalyzer())
+blob.sentiment
